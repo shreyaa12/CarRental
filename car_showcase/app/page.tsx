70804cacecc6431b45006873d857f2058499f892
Shreya Gokani <33130410+shreyaa12@users.noreply.github.com>
2023-10-22 21:07:19 -0700

    Update page.tsx

--- car_showcase/app/page.tsx
+++ car_showcase/app/page.tsx
@@ -24,8 +24,8 @@ export default async function Home() {
       <div className="home__filters">
         <SearchBar/>
         <div className="home__filter-container">
-          <CustomFilter title="fuel"/>
-          <CustomFilter title="year"/>
+{/*           <CustomFilter title="fuel"/>
+          <CustomFilter title="year"/> */}
         </div>
       </div>
 
